8665a2a102caff0bee4060f61e8264e1f1e9a41a
7ven <alexanderrr416@gmail.com>
2023-06-23 15:19:52 -0400

    Create an about me section which is optionally displayed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
 import avatar from "../images/avatar.webp";
 import {
@@ -7,23 +8,94 @@ import {
   HeroWrapper,
   Highlight,
   Separator,
+  AvatarAboutWrapper,
+  AvatarWrapper,
+  AboutWrapper,
+  AboutBtn,
+  AboutHeading,
+  ExpandAboutBtn,
 } from "../styles/styles.Hero";
 
+const aboutMe = [
+  "Here is my story. I am an enthusiastic individual who is passionate about continuous growth and learning. With an insatiable curiosity for new skills and technologies, I embrace every opportunity to expand my knowledge with a focus towards frontend development. My goal is to be persistent in my ability to create pleasant user experiences . ",
+  "Although my professional background lies within the food industry, I discovered a passion for web development that sparked a career transition. Everyday, I eagerly embrace new concepts and techniques, striving to improve. The challenge of pushing boundaries and exploring new possibilities excites me, and I am committed to my journey as a aspiring developer.",
+  "The opportunity to collaborate and work with others, in a web development environment, is something I look forward to. I genuinely enjoy meeting new people, fostering meaningful connections, and collectively working towards a common goal.",
+];
+
 export const Hero = () => {
+  const [showAboutMe, setShowAboutMe] = useState(false);
+  const [expandAboutMe, setExpandAboutMe] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const handleButtonClick = () => {
+    setShowAboutMe(!showAboutMe);
+    setExpandAboutMe(false);
+  };
+
+  const conditionalRenderAbout =
+    windowWidth > 768 ? (
+      aboutMe.map((paragraph, index) => <p key={index}>{paragraph}</p>)
+    ) : expandAboutMe ? (
+      aboutMe.map((paragraph) => <p>{paragraph}</p>)
+    ) : (
+      <p>{aboutMe[0]}</p>
+    );
+
+  const conditionalRenderBtn =
+    windowWidth < 768 ? (
+      <ExpandAboutBtn onClick={() => setExpandAboutMe(!expandAboutMe)}>
+        {expandAboutMe ? "Show Less" : "Show more"}
+      </ExpandAboutBtn>
+    ) : null;
+
   return (
-    <HeroWrapper>
-      <div>
-        <H1>
-          Hi my name is Justin Alexander,{" "}
-          <Highlight>Frontend Web Developer</Highlight>
-        </H1>
-        <Separator />
-        <HeroIconWrapper>
-          <FaGithubSquare color="#fff" size="65px" />
-          <FaLinkedin color="#fff" size="65px" />
-        </HeroIconWrapper>
-      </div>
-      <Avatar src={avatar} alt="Avatar of Justin" />
-    </HeroWrapper>
+    <>
+      <HeroWrapper>
+        <div>
+          <H1>
+            Hi my name is Justin Alexander,{" "}
+            <Highlight>Frontend Web Developer</Highlight>
+          </H1>
+          <Separator />
+          <HeroIconWrapper>
+            <FaGithubSquare color="#fff" size="65px" />
+            <FaLinkedin color="#fff" size="65px" />
+            <AboutBtn onClick={handleButtonClick}>About Me</AboutBtn>
+          </HeroIconWrapper>
+        </div>
+        <AvatarAboutWrapper>
+          <AvatarWrapper
+            style={{
+              opacity: showAboutMe ? "0" : "1",
+              zIndex: showAboutMe ? "0" : "1",
+            }}
+          >
+            <Avatar src={avatar} alt="Avatar of Justin" />
+          </AvatarWrapper>
+          <AboutWrapper
+            style={{
+              opacity: showAboutMe ? "1" : "0",
+              zIndex: showAboutMe ? "1" : "0",
+            }}
+          >
+            <AboutHeading>About me</AboutHeading>
+            {conditionalRenderAbout}
+            {conditionalRenderBtn}
+          </AboutWrapper>
+        </AvatarAboutWrapper>
+      </HeroWrapper>
+    </>
   );
 };
--- src/styles/styles.Hero.ts
+++ src/styles/styles.Hero.ts
@@ -6,7 +6,12 @@ export const HeroWrapper = styled.section`
   ${Grid1By2}
   ${containerLg}
 ${containerCenter}
-min-height:100vh
+min-height:100vh;
+  margin-bottom: 6rem;
+
+  @media screen and (min-width: 768px) {
+    margin-bottom: 0;
+  }
 `;
 
 export const H1 = styled.h1`
@@ -21,9 +26,14 @@ export const Highlight = styled.span`
 export const Avatar = styled.img`
   border-radius: 50%;
   grid-row: 1;
+
   max-width: 30rem;
   box-shadow: 0 20px 10px 0 #000;
+  max-width: 100%;
 
+  @media screen and (min-width: 500px) {
+    max-width: 40rem;
+  }
   @media screen and (min-width: 768px) {
     grid-column: 2;
     max-width: 100%;
@@ -39,4 +49,71 @@ export const HeroIconWrapper = styled.div`
   gap: 1.8rem;
   justify-content: center;
   margin-top: 2rem;
+`;
+
+export const AvatarAboutWrapper = styled.div`
+  display: grid;
+  justify-items: center;
+  grid-template-rows: 100%;
+  grid-row: 1;
+  @media screen and (min-width: 768px) {
+    grid-column: 2;
+  }
+`;
+
+export const AvatarWrapper = styled.div`
+  grid-area: 1/2;
+  align-self: center;
+  transition: opacity 0.2s ease-in;
+`;
+
+export const AboutWrapper = styled.div`
+  color: #fff;
+  line-height: 1.6;
+  padding: 3rem 3rem 0;
+  font-size: 1.6rem;
+  transition: opacity 0.5s;
+  grid-area: 1/2;
+  z-index: 1;
+
+  @media screen and (min-width: 500px) {
+    font-size: 2rem;
+  }
+
+  @media screen and (min-width: 1100px) {
+    font-size: 2.3rem;
+  }
+`;
+
+export const AboutHeading = styled.h2`
+  ${highlightText}
+`;
+
+export const AboutBtn = styled.button`
+  grid-area: 2/2;
+  outline: 0;
+  border: 0;
+  background: #6200f5;
+  color: #fff;
+  font-size: 1.6rem;
+  font-weight: 700;
+  padding: 2rem 3rem;
+  border-radius: 5px;
+  align-self: center;
+  transition: 0.4s;
+  cursor: pointer;
+
+  &:hover {
+    scale: 1.1;
+  }
+`;
+
+export const ExpandAboutBtn = styled.button`
+  outline: 0;
+  border: 0;
+  color: #fff;
+  background: #6200f5;
+  padding: 1rem 2rem;
+  border-radius: 5px;
+  cursor: pointer;
 `;
