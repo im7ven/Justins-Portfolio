ff15ebb0840598f7b023cea8b84298d068025173
7ven <alexanderrr416@gmail.com>
2023-06-29 19:47:20 -0400

    Build a contact form.

---
+++ src/components/Contact/ContactForm.tsx
@@ -0,0 +1,134 @@
+import { FieldValues, useForm } from "react-hook-form";
+import axios from "axios";
+import {
+  FormBtn,
+  FormInput,
+  FormLabel,
+  FormTextarea,
+  Form,
+  FormErrors,
+  LabelInputGroup,
+  SubmittedMsg,
+  //   SubmittedMsg,
+} from "../../styles/styles-Contact/styles-ContactForm";
+import { useState } from "react";
+
+interface FormValues {
+  firstName: string;
+  lastName: string;
+  email: string;
+  comment: string;
+}
+
+export const ContactForm = () => {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [submittedMsg, setSubmittedMsg] = useState("");
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>();
+  const url = "https://formspree.io/f/xgejwnwn";
+  const onSubmit = (data: FieldValues) => {
+    axios
+      .post(url, data)
+      .then((res) => {
+        if (res.status === 200) {
+          setSubmittedMsg(
+            "Thank you, the form has successfully been submitted."
+          );
+          setIsSubmitted(true);
+        }
+      })
+      .catch(() => {
+        setSubmittedMsg(
+          "An error occurred while submitting the form. Please try again later."
+        );
+      });
+  };
+
+  return (
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <LabelInputGroup>
+        <FormLabel htmlFor="first-name">First Name</FormLabel>
+        <FormInput
+          {...register("firstName", {
+            required: true,
+            minLength: 3,
+            maxLength: 18,
+          })}
+          id="first-name"
+          type="text"
+          name="firstName"
+        />
+        {errors.firstName?.type === "required" && (
+          <FormErrors>The name field is required.</FormErrors>
+        )}
+        {errors.firstName?.type === "minLength" && (
+          <FormErrors>Minimum of 3 characters.</FormErrors>
+        )}
+        {errors.firstName?.type === "maxLength" && (
+          <FormErrors>Maximum of 18 characters.</FormErrors>
+        )}
+      </LabelInputGroup>
+
+      <LabelInputGroup>
+        <FormLabel htmlFor="last-name">Last Name</FormLabel>
+        <FormInput
+          {...register("lastName", {
+            maxLength: 18,
+          })}
+          id="last-name"
+          type="text"
+          name="lastName"
+        />
+
+        {errors.lastName?.type === "maxLength" && (
+          <FormErrors>Maximum of 18 characters.</FormErrors>
+        )}
+      </LabelInputGroup>
+
+      <LabelInputGroup>
+        <FormLabel htmlFor="email">Email</FormLabel>
+        <FormInput
+          {...register("email", {
+            required: true,
+            pattern: {
+              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i,
+              message: "Invalid email address",
+            },
+          })}
+          id="email"
+          type="text"
+          name="email"
+        />
+        {errors.email?.type === "required" && (
+          <FormErrors>The email field is required.</FormErrors>
+        )}
+        {errors.email && <FormErrors>{errors.email.message}</FormErrors>}
+      </LabelInputGroup>
+
+      <LabelInputGroup>
+        <FormLabel htmlFor="comment">Comment</FormLabel>
+        <FormTextarea
+          {...register("comment", { required: true, minLength: 3 })}
+          id="comment"
+          rows={6}
+          cols={20}
+          name="comment"
+        ></FormTextarea>
+        {errors.comment?.type === "required" && (
+          <FormErrors>Comment field is required.</FormErrors>
+        )}
+        {errors.comment?.type === "minLength" && (
+          <FormErrors>Minimum of 3 characters.</FormErrors>
+        )}
+      </LabelInputGroup>
+
+      {isSubmitted ? null : <FormBtn type="submit">Send</FormBtn>}
+
+      {submittedMsg && <SubmittedMsg>{submittedMsg}</SubmittedMsg>}
+    </Form>
+  );
+};
---
+++ src/styles/styles-Contact/styles-ContactForm.ts
@@ -0,0 +1,81 @@
+import styled from "styled-components";
+import { highlightText } from "../styles.typograhphy";
+
+export const Form = styled.form`
+  background: #232323;
+  padding: 5rem 3rem;
+  border-radius: 20px;
+  width: 80%;
+  position: relative;
+  border-right: 3px solid #e4ba00;
+  border-left: 3px solid #e4ba00;
+`;
+
+export const FormLabel = styled.label`
+  display: block;
+  ${highlightText}
+  font-size: 1.8rem;
+  font-weight: 700;
+  margin-bottom: 0.3rem;
+
+  @media screen and (min-width: 768px) {
+    font-size: 2.4rem;
+  }
+`;
+
+export const FormInput = styled.input`
+  width: 100%;
+  box-sizing: border-box;
+  outline: 0;
+  border: 0;
+  padding: 1rem;
+  font-size: 1.6rem;
+  border-radius: 5px;
+
+  @media screen and (min-width: 768px) {
+    font-size: 2.2rem;
+  }
+`;
+
+export const FormTextarea = styled.textarea`
+  padding: 1rem;
+  box-sizing: border-box;
+  border-radius: 5px;
+  width: 100%;
+  font-size: 1.6rem;
+
+  @media screen and (min-width: 768px) {
+    font-size: 2.2rem;
+  }
+`;
+
+export const FormBtn = styled.button`
+  padding: 1.5rem 5rem;
+  outline: 0;
+  border: 0;
+  display: block;
+  color: #fff;
+  font-size: 1.7rem;
+  border-radius: 5px;
+  font-weight: 700;
+  background: #6200f5;
+  margin: 0 auto;
+  cursor: pointer;
+`;
+
+export const SubmittedMsg = styled.p`
+  margin-top: 3rem;
+  text-align: center;
+  font-size: 2rem;
+  color: #fff;
+  font-style: italic;
+`;
+
+export const FormErrors = styled.p`
+  color: red;
+  font-size: 1.6rem;
+`;
+
+export const LabelInputGroup = styled.div`
+  margin-bottom: 2.5rem;
+`;
