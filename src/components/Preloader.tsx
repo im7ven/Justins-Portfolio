9067c18da48ecfc2610cb95bfdcb4c5432df928b
Justin <alexanderrr416@gmail.com>
2023-09-25 13:12:48 -0400

    Refactor: Preload duration.

--- src/components/Preloader.tsx
+++ src/components/Preloader.tsx
@@ -13,7 +13,8 @@ export const Preloader = () => {
   const startAnimation = async () => {
     await containerControls.start({
       scaleX: 0,
-      transition: { duration: 1.2 },
+      scaleY: 0,
+      transition: { duration: 0.3 },
     });
   };
 
@@ -39,7 +40,7 @@ export const Preloader = () => {
       transition: { type: "spring", damping: 10, stiffness: 100 },
     });
 
-    await new Promise((resolve) => setTimeout(resolve, 1800));
+    await new Promise((resolve) => setTimeout(resolve, 1000));
 
     textControls3.start({
       y: 0,
@@ -56,38 +57,43 @@ export const Preloader = () => {
     });
     await new Promise((resolve) => setTimeout(resolve, 200));
     textControls5.start({
+      overflow: "hidden",
       y: 0,
       opacity: 1,
       scale: 1,
       transition: { type: "spring", damping: 10, stiffness: 100 },
     });
 
-    await new Promise((resolve) => setTimeout(resolve, 2500));
+    await new Promise((resolve) => setTimeout(resolve, 2000));
     textControls1.start({
       y: -1000,
-      //   transition: { duration: 0.3 },
     });
     textControls2.start({
       y: -1000,
-      //   transition: { duration: 0.3 },
     });
     textControls3.start({
       y: -1000,
-      //   transition: { duration: 0.3 },
     });
     textControls4.start({
       y: -1000,
-      //   transition: { duration: 0.3 },
     });
     textControls5.start({
       y: -1000,
-      //   transition: { duration: 0.3 },
     });
   };
 
   useEffect(() => {
+    window.scrollTo(0, 0);
+    document.body.classList.add("no-scroll");
     startTextAnimation();
-    setTimeout(startAnimation, 6500);
+    setTimeout(() => {
+      document.body.classList.remove("no-scroll");
+    }, 5000);
+    setTimeout(startAnimation, 5200);
+
+    return () => {
+      document.body.classList.remove("no-scroll");
+    };
   }, []);
 
   return (
